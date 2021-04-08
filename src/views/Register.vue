405e08766a7ea756652d03f55de4ac3f2903bc55
williamAstudillo <wsastudilloa@gmail.com>
2021-04-08 10:49:01 -0500

    Link telegram in register fix

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -31,7 +31,7 @@
               <span class="register-span">
                 <h2>¡Contáctanos!</h2>
                 <p class="register-foot">
-                  Encuentranos en telegram como<a href="https://web.telegram.org/#/im?p=g571607928" class="ml-1">Tropykus finance</a>
+                  Encuentranos en telegram como<a href="https://web.telegram.org/#/im?p=g571607928" target="_blank" class="ml-1">Tropykus finance</a>
                 </p>
               </span>
               <v-btn color="#4CB163" @click="register" width="40%"> Registarme </v-btn>
