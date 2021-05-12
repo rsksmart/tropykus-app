de6356fd8e1d92f6a2702dc0afe7231141861dd8
Anni Piragauta <aapiragautau@unal.edu.co>
2021-05-12 15:20:18 -0500

    Adding automatic setup for RSK Testnet MetaMask
    	I've added automatic configuration of RSK Testnet network
    	from MetaMask wallet, in order to relieve the user for this
    	responsability.

--- src/store/modules/Session.js
+++ src/store/modules/Session.js
@@ -3,30 +3,54 @@ import * as constants from '@/store/constants';
 import Vue from 'vue';
 import { ethers } from 'ethers';
 
-if (window.ethereum) {
-  window.ethereum.on('accountsChanged', () => {
-    store.dispatch(constants.SESSION_CONNECT_WEB3);
-  });
-  window.ethereum.on('chainChanged', () => {
-    store.dispatch(constants.SESSION_CONNECT_WEB3);
-  });
-}
-
 const state = {
   walletAddress: undefined,
   account: undefined,
   wallet: undefined,
   chainId: 31,
+  provider: undefined,
 };
+
+if (window.ethereum) {
+  window.ethereum.on('accountsChanged', () => {
+    store.dispatch(constants.SESSION_CONNECT_WEB3, state.wallet);
+  });
+  window.ethereum.on('chainChanged', () => {
+    store.dispatch(constants.SESSION_CONNECT_WEB3, state.wallet);
+  });
+}
 
 const actions = {
   [constants.SESSION_CONNECT_WEB3]: async ({ commit, dispatch }, wallet) => {
+    let provider = null;
     if (window.ethereum) {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      provider = window.ethereum;
+      if (wallet === constants.WALLET_LIQUALITY && window.ethereum.isLiquality) {
+        provider = window.rsk;
+      }
+      if (wallet === constants.WALLET_METAMASK && window.ethereum.isMetaMask) {
+        await window.ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: '0x1f',
+              chainName: 'RSK Testnet',
+              nativeCurrency: {
+                name: 'Test RBTC',
+                symbol: 'tRBTC',
+                decimals: 18,
+              },
+              rpcUrls: ['https://public-node.testnet.rsk.co'],
+              blockExplorerUrls: ['https://explorer.testnet.rsk.co'],
+            },
+          ],
+        });
+      }
       // eslint-disable-next-line no-multi-assign
-      Vue.prototype.$web3 = Vue.web3 = new ethers.providers.Web3Provider(window.ethereum);
+      Vue.prototype.$web3 = Vue.web3 = new ethers.providers.Web3Provider(provider);
       const account = await Vue.web3.getSigner();
-      const walletAddress = await account.getAddress();
+      const [walletAddress] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      commit(constants.SESSION_SET_PROPERTY, { provider });
       commit(constants.SESSION_SET_PROPERTY, { account });
       commit(constants.SESSION_SET_PROPERTY, { walletAddress });
       commit(constants.SESSION_SET_PROPERTY, { wallet });
@@ -45,16 +69,16 @@ const actions = {
     }
   },
   [constants.SESSION_DISCONNECT_WALLET]: ({ commit }) => {
+    commit(constants.SESSION_SET_PROPERTY, { walletAddress: undefined });
+    commit(constants.SESSION_SET_PROPERTY, { account: undefined });
+    commit(constants.SESSION_SET_PROPERTY, { wallet: undefined });
+    commit(constants.SESSION_SET_PROPERTY, { chainId: 31 });
     const web3 = new ethers.providers.JsonRpcProvider(process.env.VUE_APP_RSK_NODE);
     const format = web3.formatter.formats;
     format.receipt.root = format.receipt.logsBloom;
     Object.assign(web3.formatter, { format });
     // eslint-disable-next-line no-multi-assign
     Vue.prototype.$web3 = Vue.web3 = web3;
-    commit(constants.SESSION_SET_PROPERTY, { walletAddress: undefined });
-    commit(constants.SESSION_SET_PROPERTY, { account: undefined });
-    commit(constants.SESSION_SET_PROPERTY, { wallet: undefined });
-    commit(constants.SESSION_SET_PROPERTY, { chainId: 31 });
   },
 };
 
