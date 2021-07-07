bd19a58fd57ff040a6b38acc21297532d2646980
Carlos Rosado <krlos_1594@yahoo.com>
2021-07-07 17:57:10 -0600

    Deposit calculater
        I dindn't found the error

--- src/components/deposits/Deposit.vue
+++ src/components/deposits/Deposit.vue
@@ -53,8 +53,7 @@
                   <v-text-field type="number" dark class="h1-title text-info pa-0"
                     dense full-width single-line flat height="62"
                     v-model="amount"
-                    :rules="[rules.leverage, rules.minBalance, rules
-                        .marketCash, rules.supplyBalance]"
+                    :rules="[rules.leverage, rules.minBalance]"
                     :placeholder="'0 ' + select.underlyingSymbol"
                   />
                 </v-col>
@@ -71,9 +70,12 @@
             <v-col md="4">
               <div class="p1-descriptions text-info mb-1">{{ $t('deposit.description3')}}</div>
               <div class="p2-reading-values text-info">
-                {{tokenBalance | formatDecimals}} {{select.underlyingSymbol}}
+                {{tokenBalance | formatDecimals(select.underlyingSymbol) }}
+                {{select.underlyingSymbol}}
               </div>
-              <div class="p3-USD-values text-info">$50.000 USD</div>
+              <div class="p3-USD-values text-info">
+                {{tokenBalanceUsd | formatPrice}}
+              </div>
             </v-col>
 
             <v-col md="8">
@@ -139,8 +141,12 @@
               <div class="p1-descriptions text-info mb-1">
                 {{ $t('deposit.calculator.description1')}}
               </div>
-              <div class="p2-reading-values text-info">0 RBTC</div>
-              <div class="p3-USD-values text-info">$0 USD</div>
+              <div class="p2-reading-values text-info">
+                 {{ possibleEarnings | formatDecimals }} {{ info.underlyingSymbol }}
+              </div>
+              <div class="p3-USD-values text-info">
+                {{ possibleEarningsUSD | formatPrice }} USD
+              </div>
             </v-col>
 
             <v-col md="8">
@@ -151,10 +157,15 @@
                 <v-col class="pa-0">
                   <v-text-field type="number" dark class="h1-title text-info pa-0"
                     dense full-width single-line flat height="62"
+                    v-model="amountEarning"
                   />
                 </v-col>
               </v-row>
             </v-col>
+          </v-row>
+          <v-row>
+            <!-- {{returnYear}} -->
+            {{sliderYear}}
           </v-row>
 
           <v-row class="mb-10">
@@ -162,8 +173,12 @@
               <div class="p1-descriptions text-info mb-1">
                 {{ $t('deposit.calculator.description3')}}
               </div>
-              <div class="p2-reading-values text-info">0 RBTC</div>
-              <div class="p3-USD-values text-info">$0 USD</div>
+              <div class="p2-reading-values text-info">
+                {{ possibleEarningsPlusDeposit | formatDecimals }} {{ info.underlyingSymbol }}
+              </div>
+              <div class="p3-USD-values text-info">
+                {{ possibleEarningsPlusDepositUSD | formatPrice }} USD
+              </div>
             </v-col>
 
             <v-col md="8">
@@ -172,19 +187,20 @@
               </div>
               <v-slider
                 class="mt-3 deposit-slider"
-                min="0"
-                max="100"
+                min="1"
+                max="5"
                 color="#4CB163"
                 track-color=" #4CB163"
                 tick-size="10"
                 thumb-label
+                v-model="sliderYear"
               />
               <v-row class="ma-0">
                 <v-col class="pa-0 d-flex justify-start">
-                  <span class="p1-descriptions">1%</span>
+                  <span class="p1-descriptions">1 year</span>
                 </v-col>
                 <v-col class="pa-0 d-flex justify-end mr-3">
-                  <span class="p1-descriptions">100%</span>
+                  <span class="p1-descriptions">5 year</span>
                 </v-col>
               </v-row>
             </v-col>
@@ -206,13 +222,17 @@
           />
         </template>
 
+        <template v-if="isLoading">
+          <loading :showModal="isLoading" :data="infoLoading" @closed="closeDialog">
+          </loading>
+        </template>
       </div>
     </div>
 </template>
 <script>
-// import SupplyRedeem from '@/components/dialog/SupplyRedeem.vue';
 import { mapState } from 'vuex';
 import ConnectWallet from '@/components/dialog/ConnectWallet.vue';
+import Loading from '@/components/modals/Loading.vue';
 import * as constants from '@/store/constants';
 import {
   CToken,
@@ -220,12 +240,12 @@ import {
   Market,
   Comptroller,
 } from '@/middleware';
-// import WalletIcon from '@/assets/icons/wallet.svg';
-// import PigIcon from '@/assets/icons/pig.svg';
 
 export default {
+  name: 'Deposit',
   components: {
     ConnectWallet,
+    Loading,
   },
   data() {
     return {
@@ -234,20 +254,27 @@ export default {
       comptroller: undefined,
       getMarkets: [],
       market: null,
+      isLoading: false,
+      infoLoading: {
+        amount: '',
+        symbol: '',
+        wallet: true,
+        loading: true,
+        success: null,
+      },
       select: {
         symbol: '',
         img: '',
       },
       showModalConnectWallet: false,
       sliderAmountPercentage: 0,
+      sliderYear: 0,
       amount: null,
+      amountEarning: null,
       marketAddress: null,
       info: {
         underlyingSymbol: null,
-        cash: null,
         rate: null,
-        totalBorrows: null,
-        totalBalance: null,
         underlyingPrice: null,
         underlyingBalance: null,
         liquidity: null,
@@ -258,16 +285,12 @@ export default {
       data: {
         underlyingBalance: null,
         supplyBalance: null,
+        price: null,
       },
       rules: {
-        leverage: () => (this.inSupplyMenu ? this.info.borrowBalance <= 0
-          : true) || this.$t('dialog.supply-redeem.rule1'),
-        minBalance: () => (this.inSupplyMenu ? Number(this.amount) <= Number(this
+        leverage: () => (this.account ? this.info.borrowBalance <= 0 : true) || this.$t('dialog.supply-redeem.rule1'),
+        minBalance: () => (this.account ? Number(this.amount) <= Number(this
           .data.underlyingBalance) : true) || this.$t('dialog.supply-redeem.rule2'),
-        supplyBalance: () => (!this.inSupplyMenu ? Number(this.amount) <= Number(this
-          .data.supplyBalance) : true) || this.$t('dialog.supply-redeem.rule3'),
-        marketCash: () => (!this.inSupplyMenu ? Number(this.amount) <= Number(this
-          .info.cash) : true) || this.$t('dialog.supply-redeem.rule4'),
       },
     };
   },
@@ -278,13 +301,38 @@ export default {
       walletAddress: (state) => state.Session.walletAddress,
       account: (state) => state.Session.account,
     }),
+    tokenBalanceUsd() {
+      return this.data.underlyingBalance * this.data.price;
+    },
     tokenBalance() {
       return this.data.underlyingBalance;
     },
+    possibleEarnings() {
+      return +this.amountEarning ? ((this.amountEarning * this.supplyRate) + (this.info
+        .supplyBalance * this.supplyRate) * this.sliderYear) : (this.info
+        .interestBalance * this.sliderYear);
+    },
+    possibleEarningsUSD() {
+      return this.possibleEarnings * this.info.underlyingPrice;
+    },
+    possibleEarningsPlusDeposit() {
+      if (+this.amountEarning) {
+        return this.possibleEarnings > this.info.interestBalance ? (+this.info
+          .supplyBalance + +this.amountEarning + +this.possibleEarnings) : (this.info
+          .supplyBalance * this.sliderYear);
+      }
+      return this.info.supplyBalance + this.possibleEarnings;
+    },
+    possibleEarningsPlusDepositUSD() {
+      return this.possibleEarningsPlusDeposit * this.info.underlyingPrice;
+    },
+    supplyRate() {
+      return this.info.rate / 100;
+    },
+    // returnYear
   },
   watch: {
     markets() {
-      console.log('markets watcher:', this.markets);
       if (this.markets.length > 3) this.getMarketsInfo();
     },
     $route() {
@@ -300,37 +348,43 @@ export default {
         this.showModalConnectWallet = true;
         return;
       }
-      this.allMarkets = await this.comptroller.allMarkets;
-      if (assetsIn.indexOf(this.marketAddress) === -1) {
-        await this.comptroller.enterMarkets(this.account, this.allMarkets);
-      }
-      // this.txSummaryDialog = true; // TODO
-      this.showWaiting();
+      this.isLoading = true;
+      this.infoLoading.loading = true;
+      this.infoLoading.wallet = true;
       await this.market.supply(this.account, this.amount)
         .then(() => {
+          this.infoLoading.wallet = false;
           this.market.wsInstance.on('Mint', (from) => {
             if (from === this.walletAddress) {
-              this.showSuccess();
-              this.updateMarketInfo();
+              if (!this.isLoading) {
+                this.isLoading = true;
+              }
+              this.getMarket();
+              this.infoLoading.loading = false;
+              this.infoLoading.amount = this.amount;
+              this.infoLoading.symbol = this.select.symbol;
             }
           });
         })
         .catch(this.showError);
+
+      this.market.wsInstance.on('Failure', (from, to, amount, event) => {
+        console.info(`Failure from ${from} Event: ${JSON.stringify(event)}`);
+        const { error, detail, info } = event.args;
+        console.log(`Error: ${error}, detail: ${detail}, info: ${info}`);
+        if (this.walletAddress === from) {
+          // this.showError();
+          console.log('error');
+        }
+      });
     },
     async getMarketsInfo() {
       this.getMarkets = [];
       await this.markets.map(async (market) => {
-        // const subsidy = await market.getSubsidyFound();
         try {
           const data = {};
           data.symbol = await market.symbol;
           data.underlyingSymbol = await market.underlyingAssetSymbol();
-          data.marketAddress = market.marketAddress;
-          // data.reserve_usd = await market.reservesInUSD(this.chainId);
-          // data.name = await market.underlyingAssetName();
-          // data.price = await market.underlyingCurrentPrice(this.chainId);
-          // data.reserves = await market.getReserves();
-
           data.img = await this.db
             .collection('markets-symbols')
             .doc(data.symbol)
@@ -346,13 +400,8 @@ export default {
     },
     async updateMarketInfo() {
       this.info.underlyingSymbol = await this.market.underlyingAssetSymbol();
-      this.info.cash = await this.market.getCash();
-      this.info.symbol = await this.market.symbol;
-      this.info.rate = this.inBorrowMenu
-        ? await this.market.borrowRateAPY()
-        : await this.market.supplyRateAPY();
+      this.info.rate = await this.market.supplyRateAPY();
       this.info.rate = this.info.rate.toFixed(3);
-      this.info.totalBorrows = await this.market.totalBorrowsInUnderlying();
       // this.getSymbolImg();
       if (this.chainId) {
         this.info.underlyingPrice = await this.market.underlyingCurrentPrice(this.chainId);
@@ -360,28 +409,18 @@ export default {
       if (this.walletAddress) {
         this.info.underlyingBalance = await this.market
           .balanceOfUnderlyingInWallet(this.account);
-        this.info.interestBalance = this.inBorrowMenu
-          ? await this.market.getDebtInterest(this.walletAddress)
-          : await this.market.getEarnings(this.walletAddress);
-        this.info.liquidity = await this.comptroller.getAccountLiquidity(this.walletAddress);
         this.info.supplyBalance = await this.market
           .currentBalanceOfCTokenInUnderlying(this.walletAddress);
         this.info.borrowBalance = await this.market
           .borrowBalanceCurrent(this.walletAddress);
-        this.info.interestBalance = this.inBorrowMenu
-          ? await this.market.getDebtInterest(this.walletAddress)
-          : await this.market.getEarnings(this.walletAddress);
-        this.info.totalBalance = this.inBorrowMenu
-          ? this.info.borrowBalance
-          : await this.market.currentBalanceOfCTokenInUnderlying(this.walletAddress);
+        this.info.interestBalance = await this.market.getEarnings(this.walletAddress);
       }
     },
     async refresh() {
-      console.log('refres');
       if (this.account) {
         this.data.underlyingBalance = await this.market
           .balanceOfUnderlyingInWallet(this.account);
-        console.log(this.data.underlyingBalance);
+        this.data.price = await this.market.underlyingCurrentPrice(this.chainId);
       }
       this.data.supplyBalance = await this.market
         .currentBalanceOfCTokenInUnderlying(this.walletAddress);
@@ -428,13 +467,14 @@ export default {
       this.sliderAmountPercentage = 0;
       this.amount = null;
     },
+    closeDialog() {
+      this.isLoading = false;
+    },
   },
   created() {
     this.comptroller = new Comptroller(this.chainId);
     this.getMarket();
     this.getMarketsInfo();
-  },
-  updated() {
   },
 };
 </script>
--- src/components/modals/Loading.vue
+++ src/components/modals/Loading.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    <v-dialog v-model="dialog" width="414" overlay-opacity="0.6" overlay-color="#000">
+  <div class="aqui">
+    <v-dialog v-model="dialog" content-class="d-loading" overlay-opacity="0.6" overlay-color="#000">
       <v-card class="dialog-loading ma-0 secondary-color d-flex align-center"
         v-click-outside="closeDialog">
         <v-icon class="btn-close-dialog" @click="closeDialog">mdi-close</v-icon>
-        <div class="row ma-0 d-flex flex-column align-center">
-          <v-progress-circular indeterminate color="#41A255" :width="10" :size="90"/>
-          <div class="text-primary s1-pop-ups text-description mt-9">
+        <div v-if="data.loading" class="row ma-0 d-flex flex-column align-center">
+          <v-progress-circular class="progress-loading"
+            indeterminate color="#41A255" :width="6" :size="80"/>
+          <div v-if="data.wallet" class="text-primary s1-pop-ups text-description mt-9">
             Esperando la confirmación de la transacción en tu billetera
           </div>
+          <div v-else class="text-primary s1-pop-ups text-description mt-9">
+            Esperando la confirmación de la red
+          </div>
+        </div>
+        <div v-else class="row ma-0 d-flex flex-column align-center">
+          <v-img width="150" height="150" src="@/assets/dialog/success.svg" alt=""/>
+          <div class="text-primary s1-pop-ups text-deposit">
+            Has depositado <br />
+            {{data.amount | formatDecimals(data.symbol) }}
+            {{data.symbol}}
+          </div>
+          <v-btn @click="closeDialog" class="btn btn-primary">
+            <span class="b1-main white--text">Continuar</span>
+          </v-btn>
         </div>
         <!-- <img src="@/assets/dialog/background.svg" alt=""> -->
       </v-card>
@@ -18,16 +33,19 @@
 <script>
 
 export default {
-  name: 'Empty',
+  name: 'Loading',
   data() {
     return {
       dialog: this.showModal,
-      checkbox: false,
     };
   },
   props: {
     showModal: {
       type: Boolean,
+    },
+    data: {
+      type: Object,
+      require: true,
     },
   },
   watch: {
@@ -37,7 +55,6 @@ export default {
   },
   methods: {
     closeDialog() {
-      // this.dialog = false;
       this.$emit('closed');
     },
   },
