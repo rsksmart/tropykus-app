f76c5b0c49f1a76404a0d9fa55d655020ed5d96d
Anni Piragauta <aapiragautau@unal.edu.co>
2021-08-13 13:51:33 -0500

    Fixing Rif oracle address

--- src/middleware/contracts/constants.js
+++ src/middleware/contracts/constants.js
@@ -28,7 +28,7 @@ export const addresses = {
     RIF: '0x19f64674d8a5b4e652319f5e239efd3bc969a1fe',
     DOC: '0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0',
     rUSDT: '0x4cfe225ce54c6609a525768b13f7d87432358c57',
-    RifOracle: '0x9e84450e31bc6c34b74f0e829449c59bb3bc6dde',
+    RifOracle: '0x4cfe225ce54c6609a525768b13f7d87432358c57',
     RbtcOracle: '0x0c1c2c3fc56e812cb5d6356d38890808ec0336c9',
     docOracle: '0x1a53651bf01f8382bf6d315194e451dc3854c742',
     usdtOracle: '0xa34941051d1b6931580bbeeb7b4025040d60b2c0',
@@ -51,11 +51,6 @@ export const addresses = {
     kSAT: '0xd1edae988e9afaf6da1b44a4d0c40c0ea03ce9c6',
     tropykusLens: '0x0a9ff3df618aa6c3bd727efe491e5f17faaa82bc',
     whitelist: '0x60e7bbd1c4c6c77d829038137d1a306d5c21aeb1',
-    underlying: {
-      kRIF: '0x19f64674d8a5b4e652319f5e239efd3bc969a1fe',
-      kDOC: '0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0',
-      kUSDT: '0x4cfe225ce54c6609a525768b13f7d87432358c57',
-    },
   },
 };
 
